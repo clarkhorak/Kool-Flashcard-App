7f69a59b8b4a7a355ce99d41a1e49e4191867bc3
clarkhorak <clarkhorak@gmail.com>
2022-09-20 00:21:39 -0500

    Added button clicks to change between question and answer

--- app/src/main/java/com/example/koolflashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/koolflashcardapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.koolflashcardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+
+        flashcardAnswer.setOnClickListener {
+            flashcardAnswer.visibility = View.INVISIBLE
+            flashcardQuestion.visibility = View.VISIBLE
+        }
     }
 }
