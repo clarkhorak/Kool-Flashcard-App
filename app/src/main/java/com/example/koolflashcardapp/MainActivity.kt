5f859a618a7c989edc1aa25d3e5514a751b26eaf
clarkhorak <clarkhorak@gmail.com>
2022-10-01 19:40:51 -0500

    Added button clicks to change between question and answer Added Cancel button Added Option to change flashcard question/answer with user input Added save button and save logic

--- app/src/main/java/com/example/koolflashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/koolflashcardapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.koolflashcardapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +17,42 @@ class MainActivity : AppCompatActivity() {
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
         flashcardQuestion.setOnClickListener {
-            flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
+            flashcardQuestion.visibility = View.INVISIBLE
+
+            Snackbar.make(flashcardQuestion, "Question button was clicked",
+                Snackbar.LENGTH_SHORT).show()
         }
 
         flashcardAnswer.setOnClickListener {
             flashcardAnswer.visibility = View.INVISIBLE
             flashcardQuestion.visibility = View.VISIBLE
         }
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                result ->
+
+            val data: Intent? = result.data
+
+            if (data != null) {
+                val questionString = data.getStringExtra("QUESTION_KEY")
+                val answerString = data.getStringExtra("ANSWER_KEY")
+
+                flashcardQuestion.text = questionString
+                flashcardAnswer.text = answerString
+
+                Log.i("Clark: MainActivity", "question: $questionString")
+                Log.i("Clark: MainActivity", "answer: $answerString")
+            } else {
+                Log.i("Clark: MainActivity", "Returned null data from AddCardActivity")
+            }
+        }
+
+        val addQuestionButton = findViewById<ImageView>(R.id.add_question_button)
+        addQuestionButton.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+
+
     }
 }
